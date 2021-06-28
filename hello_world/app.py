ff1c477df4d6c6ee39b2cb0feafb8d1a09b3e0b2
StevenMcGown <stevenmcgown1@gmail.com>
2021-06-27 23:32:07 -0400

    fixed CORS error

---
+++ hello_world/app.py
@@ -0,0 +1,35 @@
+import json
+import boto3
+# import requests
+
+
+def lambda_handler(event, context):
+    client = boto3.client('dynamodb')
+
+    count = client.get_item(
+        TableName='counter',
+        Key={
+            'page_name' : {'S': 'resume'},
+        }
+    )
+
+    count = str(int(count['Item']['count']['N']) + 1)
+
+    response = client.put_item(
+            TableName='counter',
+            Item={
+                'page_name' : {'S': 'resume'},
+                'count' : {'N': count}
+            }
+    )
+
+    return {
+        "statusCode": 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps({
+            "Visitors": count,
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
