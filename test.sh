57a1a62fa0dc34e406fbfd73bf058d468a1b92fb
StevenMcGown <stevenmcgown1@gmail.com>
2021-07-06 12:11:52 -0400

    Automatic SAM deploy

--- test.sh
+++ test.sh
@@ -2,3 +2,9 @@ sam build && sam local invoke LambdaTestFunction
 pip install pytest
 pip install boto3
 python3 -m pytest hello_world/app_test.py
+ret=$?
+if [ "$ret" = 5 ]; then
+  echo "No tests collected.  Exiting with 0 (instead of 5)."
+  exit 0
+fi
+exit "$ret"
